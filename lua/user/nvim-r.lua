921e9e0640cfdc1eba4807770f0f7b2af9326c83
Henry Stern <108289013+henrystern@users.noreply.github.com>
2023-05-31 02:46:49 -0400

    deactivate r commands

--- lua/user/nvim-r.lua
+++ lua/user/nvim-r.lua
@@ -2,6 +2,10 @@ local M = {
   "jalvesaq/Nvim-R",
   lazy = true,
   ft = {'r', 'rmd', 'rrst', 'rnoweb', 'quarto', 'rhelp'},
+  init = function()
+    vim.g.R_disable_cmds = {'RCustomStart', 'RSaveClose', 'RClearAll', 'RSimpleComment', 'RSimpleUnComment', 'RToggleComment', 'RRightComment', 'RIndent', 'RNextRChunk', 'RPreviousRChunk', 'RNLeftPart', 'RNRightPart', 'RILeftPart', 'RIRightPart', 'RESendSelection', 'RDSendSelection', 'REDSendSelection', 'RSendMotion', 'RSendSelAndInsertOutput', 'RSendMBlock', 'RESendMBlock', 'RDSendMBlock', 'REDSendMBlock', 'RSendParagraph', 'RESendParagraph', 'RDSendParagraph', 'REDSendParagraph', 'RESendFunction', 'RDSendFunction', 'REDSendFunction', 'RShowEx', 'RShowRout', 'RViewDFs', 'RViewDFa', 'RDputObj', 'RSetwd', 'RListSpace', 'RSendChunk', 'RDSendChunk', 'RESendChunk', 'REDSendChunk', 'RSendChunkFH', 'RBibTeX', 'RBibTeXK', 'RSweave', 'RKnit', 'RMakeHTML', 'RMakePDF', 'RMakePDFK', 'RMakePDFKb', 'RMakeODT', 'RMakeWord', 'RMakeRmd', 'RMakeAll', 'ROpenPDF', 'RSyncFor', 'RGoToTeX', 'RSpinFile', 'RNextRChunk', 'RPreviousRChunk', 'RQuartoRender', 'RQuartoPreview', 'RQuartoStop', 'ROpenRefFile', 'RDebug', 'RUndebug', 'ROpenLists', 'RCloseLists'}
+  end
 }
+
 
 return M
