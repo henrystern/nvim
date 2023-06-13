15436bde5cebc40d0fc6d1fb8bf0291e1001225f
Henry Stern <henrystern@noreply.users.github.com>
2023-06-13 13:54:10 -0400

    change nvim-r options

--- ftplugin/r.vim
+++ ftplugin/r.vim
@@ -1,17 +1,6 @@
-let R_min_editor_width = 80   " Source editor should be at least 80 col
-let R_rconsole_width = 1000   " Console takes up whole width
-let R_rconsole_height = 20    " Console isn't too tall
-let R_objbr_opendf = 0        " Don't expand dataframe in obj browser
-let R_assign = 2              " 2 underscores inserts the assn. operator
-nmap , <Plug>RDSendLine       " Comma sends current line in normal mode
-vmap , <Plug>RESendSelection  " Comma sends current selection in visual mode
-
-let R_filetypes = ['r', 'rmd', 'rrst', 'rnoweb', 'quarto', 'rhelp']
-
 " let R_DEFAULT_PACKAGES = 'httpgd'
 " let R_after_start = [':hgd()', ':hgd_browse()']
 
-let R_csv_delim = ','
 
 " let R_app = "radian"
 " let R_cmd = "R"
--- lua/user/nvim-r.lua
+++ lua/user/nvim-r.lua
@@ -4,6 +4,13 @@ local M = {
   ft = {'r', 'rmd', 'rrst', 'rnoweb', 'quarto', 'rhelp'},
   init = function()
     vim.g.R_disable_cmds = {'RCustomStart', 'RSaveClose', 'RClearAll', 'RSimpleComment', 'RSimpleUnComment', 'RToggleComment', 'RRightComment', 'RIndent', 'RNextRChunk', 'RPreviousRChunk', 'RNLeftPart', 'RNRightPart', 'RILeftPart', 'RIRightPart', 'RESendSelection', 'RDSendSelection', 'REDSendSelection', 'RSendMotion', 'RSendSelAndInsertOutput', 'RSendMBlock', 'RESendMBlock', 'RDSendMBlock', 'REDSendMBlock', 'RSendParagraph', 'RESendParagraph', 'RDSendParagraph', 'REDSendParagraph', 'RESendFunction', 'RDSendFunction', 'REDSendFunction', 'RShowEx', 'RShowRout', 'RViewDFs', 'RViewDFa', 'RDputObj', 'RSetwd', 'RListSpace', 'RSendChunk', 'RDSendChunk', 'RESendChunk', 'REDSendChunk', 'RSendChunkFH', 'RBibTeX', 'RBibTeXK', 'RSweave', 'RKnit', 'RMakeHTML', 'RMakePDF', 'RMakePDFK', 'RMakePDFKb', 'RMakeODT', 'RMakeWord', 'RMakeRmd', 'RMakeAll', 'ROpenPDF', 'RSyncFor', 'RGoToTeX', 'RSpinFile', 'RNextRChunk', 'RPreviousRChunk', 'RQuartoRender', 'RQuartoPreview', 'RQuartoStop', 'ROpenRefFile', 'RDebug', 'RUndebug', 'ROpenLists', 'RCloseLists'}
+    vim.g.R_min_editor_width = 80   -- Source editor should be at least 80 col
+    vim.g.R_rconsole_width = 1000   -- Console takes up whole width
+    vim.g.R_rconsole_height = 20    -- Console isn't too tall
+    vim.g.R_objbr_opendf = 0        -- Don't expand dataframe in obj browser
+    vim.g.R_assign = 0
+    vim.g.R_rmdchunk = 0
+    vim.g.R_csv_delim = ','
   end
 }
 
