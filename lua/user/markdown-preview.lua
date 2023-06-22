afc2c87092cc9eb177c5126ce0a50d7c1feb1e0b
Henry Stern <108289013+henrystern@users.noreply.github.com>
2023-06-21 21:20:05 -0400

    remove vim-markdown

--- lua/user/markdown-preview.lua
+++ lua/user/markdown-preview.lua
@@ -8,13 +8,13 @@ function M.config()
   local wk = require "which-key"
   vim.g.mkdp_auto_close = 0
   vim.g.mkdp_command_for_global = 0
-  vim.g.mkdp_browser = 'firefox'
   vim.g.mkdp_filetypes = {"markdown", "quarto"}
+  vim.g.mkdp_browser = 'firefox'
+  vim.g.mkdp_page_title = '${name} - Live Preview'
   wk.register(
     {
       m = {
         name = 'markdown',
-        t = { ":Toc<cr>", 'ToC' }, -- this isn't part of this plugin it is  from vim-markdown
         p = { ":MarkdownPreview<cr>", 'preview' },
       },
     }, { mode = 'n', prefix = '<localleader>' }
