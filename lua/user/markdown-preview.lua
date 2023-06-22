f87755f44fb1628119e87acfeb5be0aabf96f408
Henry Stern <108289013+henrystern@users.noreply.github.com>
2023-06-21 22:42:04 -0400

    custom mdpreview styles

--- lua/user/markdown-preview.lua
+++ lua/user/markdown-preview.lua
@@ -7,10 +7,10 @@ local M = {
 function M.config()
   local wk = require "which-key"
   vim.g.mkdp_auto_close = 0
-  vim.g.mkdp_command_for_global = 0
   vim.g.mkdp_filetypes = {"markdown", "quarto"}
-  vim.g.mkdp_browser = 'firefox'
   vim.g.mkdp_page_title = '${name} - Live Preview'
+  vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/data/markdown_preview/styles.css"
+  vim.g.mkdp_browser = 'firefox'
   wk.register(
     {
       m = {
