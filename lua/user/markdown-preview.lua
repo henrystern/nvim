c05bdde91c1cd3a3ab1bdd9fb530dc18a2a6d84b
Henry Stern <108289013+henrystern@users.noreply.github.com>
2023-06-24 22:02:04 -0400

    set md port

--- lua/user/markdown-preview.lua
+++ lua/user/markdown-preview.lua
@@ -12,6 +12,7 @@ function M.config()
   vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/data/markdown_preview/styles.css"
   vim.g.mkdp_highlight_css = vim.fn.stdpath("config") .. "/data/markdown_preview/highlights.css"
   vim.g.mkdp_browser = 'firefox'
+  vim.g.mkdp_port = 6464
   wk.register(
     {
       m = {
