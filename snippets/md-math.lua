9aa6e60ae7aba48a271ef1fac6f44d5f304a98fb
Henry Stern <henrystern@users.noreply.github.com>
2024-01-18 21:07:35 -0500

    snippet updates

--- snippets/md-math.lua
+++ snippets/md-math.lua
@@ -89,9 +89,9 @@ return {
     "dm",
     fmta(
       [[
-      $$\begin{aligned}
+      $$\begin{align*}
         <>
-      \end{aligned}$$
+      \end{align*}$$
     ]],
       dl(1, l.LS_SELECT_RAW)
     )
@@ -135,6 +135,8 @@ return {
   md.A_math("=<", t "\\impliedby "),
   md.A_math("<=", t "\\le "),
   md.A_math(">=", t "\\ge "),
+  md.A_math(" or", t " \\vee "),
+  md.A_math(" and", t " \\wedge "),
 
   -- Shortcuts
   md.math("nd", t "&"),
